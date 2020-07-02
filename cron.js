4883e514cc760a1c3a5b18a5ec1e135e39fbbeab
Marcus Stewart <MMSStewart20@gmail.com>
2020-07-01 20:57:23 -0500

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const snkrBot = require("./bot");
 
-// Scheduled to run at 6:00 AM when Nike Canada usually releases a shoe.
+// Scheduled to run at 9:00 AM when Nike USA usually releases a shoe.
 cron.schedule("0 9 * * *", () => {
 	snkrBot();
 });
